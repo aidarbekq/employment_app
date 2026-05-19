ae648c8a1e243d14540d8cccd982a88357bef33f
aidarbek <aidar@ubuntu>
2026-05-19 19:21:39 +0600

    refactor: rename project directories to frontend and backend

--- employment_system-main/alumni/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import AlumniProfileViewSet
-
-router = DefaultRouter()
-router.register(r"alumni-profiles", AlumniProfileViewSet, basename="alumniprofile")
-
-urlpatterns = router.urls
--- employment_system-main/employers/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import EmployerViewSet
-
-router = DefaultRouter()
-router.register(r"employers", EmployerViewSet, basename="employer")
-
-urlpatterns = router.urls
